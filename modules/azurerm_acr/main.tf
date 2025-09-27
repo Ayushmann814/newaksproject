8625e65e773bcec6c09ff57971850ca2b70d28d2
ayushsaxena814 <ayushsaxena814@gmail.com>
2025-09-27 17:47:26 +0530

    added new name of acr

--- modules/azurerm_acr/main.tf
+++ modules/azurerm_acr/main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_container_registry" "acr" {
-  name                = "containerRegistry1"
+  name                = "ayushcontainerregistry1879"
   resource_group_name = var.resource_group_name
   location            = var.location
   sku                 = "Premium"
